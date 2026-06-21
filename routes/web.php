39b458a2945d5d9dba560339262e4e1d677c170f
Fadel Muhammad <FadelMuhammad27@users.noreply.github.com>
2026-06-21 13:07:14 +0800

    Pertemuan 11: layouting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController; 
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::resource('/user', UserController::class);
